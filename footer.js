7e3bb448d8c5a441396184877f41f0306396c6e1
asligadirova <aslisg@code.edu.az>
2022-06-13 11:36:20 +0400

    first commit

---
+++ footer.js
@@ -0,0 +1,44 @@
+// console.log("salam")
+
+function Baku() {
+    var html3 = "baku@simbrella.com";
+    var html = "Main Operations Office";
+    var html2 = "Jalil Mammadguluzadeh st. 102 A, City Point Business Centre, AZ1022, Baku, Azerbaijan";
+    document.getElementsByClassName("baku")[0].innerHTML = html;
+    document.getElementsByClassName("first-p")[0].innerHTML = html2;
+    document.getElementsByClassName("second-a")[0].innerHTML = html3
+    document.querySelector("#amsterdam").style.color = "#999999";
+    document.querySelector("#singapore").style.color = "#999999";
+    document.querySelector("#baku").style.color = "#FF931E";
+    // console.log("salam")
+    // document.getElementById("baku").onmouseover = function () { mouseOver() };
+    // document.getElementById("baku").onmouseout = function () { mouseOut() };
+
+
+
+}
+function Singapore() {
+    var html3 = "singapore@simbrella.com";
+    var html = "APAC Region Office";
+    var html2 = "1163 Tras Street, #07-02, Lian Huat Building, 079024, Singapore";
+    document.getElementsByClassName("baku")[0].innerHTML = html;
+    document.getElementsByClassName("first-p")[0].innerHTML = html2;
+    document.getElementsByClassName("second-a")[0].innerHTML = html3
+    document.querySelector("#amsterdam").style.color = "#999999";
+    document.querySelector("#singapore").style.color = "#FF931E";
+    document.querySelector("#baku").style.color = "#999999";
+    // document.getElementById("singapore").onmouseover = function () { mouseOver() };
+    // document.getElementById("singapore").onmouseout = function () { mouseOut() };
+}
+function Amsterdam() {
+    var html3 = "amsterdam@simbrella.com";
+    var html = "Headquarters";
+    var html2 = "Teleportboulevard 130, Scalehub, Unit number 1.08, 1043 EJ Amsterdam, Netherlands";
+    document.getElementsByClassName("baku")[0].innerHTML = html;
+    document.getElementsByClassName("first-p")[0].innerHTML = html2;
+    document.getElementsByClassName("second-a")[0].innerHTML = html3
+    document.querySelector("#amsterdam").style.color = "#FF931E";
+    document.querySelector("#singapore").style.color = "#999999";
+    document.querySelector("#baku").style.color = "#999999";}
+    // document.getElementById("amsterdam").onmouseover = function () { mouseOver() };
+    // document.getElementById("amsterdam").onmouseout = function () { mouseOut() };}
